312570f4931f77a0d3c910cddda2d8f5e45a0644
villalbajoaquin <villalbajoaq@gmail.com>
2024-01-03 23:54:59 -0300

    1er commit [001]

---
+++ Programas/001.c
@@ -0,0 +1,16 @@
+// 6. Estructura general de un programa en C
+
+#include<stdio.h>
+
+// Comentario de una linea
+
+/*
+   Comentario multi-linea
+*/
+
+int main(){ // Funcion Principal
+
+	printf("Hola Mundo");
+	
+	return 0;
+}
