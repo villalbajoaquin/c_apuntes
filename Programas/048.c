bba848634a7d8962939c35922e6fa9f293b41288
villalbajoaquin <villalbajoaq@gmail.com>
2024-02-27 01:43:50 -0300

    add [048] mod [README]

---
+++ Programas/048.c
@@ -0,0 +1,25 @@
+// 37. Ciclos/Bucles. Ejercicio:
+// l) Serie Fibonacci.
+
+//	1 2 3 5 8 13
+
+#include<stdio.h>
+
+int main(){
+	int n, i, x = 0, y = 1, z = 1;
+	
+	printf("Ingrese el numero de elementos: ");
+	scanf("%i", &n);
+	
+	printf("1, ");
+	
+	for(i = 1; i < n; i++){
+		z = x + y; // z = 0 + 1 = 1; z = 1 + 1 = 2  
+		x = y; // cambio x = 1;     cambio x = 1
+		y = z; // asigno y = z = 1; asigno y = z = 2
+		
+		printf("%i, ", z);
+	};
+	
+	return 0;
+}
--- README.md
+++ README.md
@@ -48,6 +48,7 @@
 - 045 (23/02): Ciclos/Bucles. Ejercicio: (i) Determinar si un numero es primo o no.
 - 046 (24/02): Ciclos/Bucles. Ejercicio: (j) Factorial de un numero.
 - 047 (26/02): Ciclos/Bucles. Ejercicio propuesto: (k) Calcular la suma de factoriales.
+- 048 (27/02): Ciclos/Bucles. Ejercicio: (l) Serie Fibonacci.
 
 * * *
 Este es un repositorio de [Joaquín Villalba](https://github.com/villalbajoaquin).
